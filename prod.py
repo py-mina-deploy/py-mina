7d96206cfb7675b4611c837ce9400c0006ce766e
Cidevant Von Goethe <cidevant@mail.ru>
2017-09-21 08:08:26 +0200

    Init commit

--- prod.py
+++ prod.py
@@ -4,5 +4,5 @@ from deploy import *
 set('user', 'deploy')
 set('hosts', ['agony.wdf.cz'])
 set('branch', 'master')
-# set('repository', 'git@git.wdf.cz:wdf-developers/watermark.git')
+set('repository', 'git@git.wdf.cz:wdf-developers/watermark.git')
 set('deploy_to', '/var/www/py-mina-test')
