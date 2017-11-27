b6bd46cf88f85f8997262a1e460a7d348ea45bf0
Cidevant Von Goethe <cidevant@mail.ru>
2017-11-27 13:36:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="py_mina",
-    version="0.1.6",
+    version="0.1.7",
     description="Python library for deploying applications on remote server",
     long_description=long_description,
     license="MIT",
