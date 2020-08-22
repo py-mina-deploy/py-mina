36ec73f0f6ce0e9f65a5261dc26cf4d05bb833c4
steven lv <steven.suzhou@gmail.com>
2020-08-22 10:06:10 +0800

    add debug info and test file

--- py_mina/subtasks/deploy.py
+++ py_mina/subtasks/deploy.py
@@ -159,6 +159,8 @@ def link_shared_paths():
 
             with cd(build_to):
                 # create_entity(directory, entity_type='directory', protected=False) # create parent directory
+                run("pwd")
+                run("test -f %s" % (relative_path))
                 run('ln -sf %s %s' % (shared_path, relative_path)) # link shared to current folder
 
 
